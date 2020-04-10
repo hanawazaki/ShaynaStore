6e85236ff4465e34245cf6b7d1d5b91f8415e5f8
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2020-04-10 20:16:39 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+   <HeaderShayna />
+   <HeroShayna />
+   <WomanBanner />
+   <InstaShayna />
+   <PartnerLogo />
+   <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanBanner from '@/components/WomanBanner.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanBanner,
+    InstaShayna,
+    PartnerLogo,
+    FooterShayna
+  }
+}
+</script>
